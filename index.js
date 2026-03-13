a86f95f5b779a011463e57a5bc7881e24f9f71b5
Mishkat Naveed <mishkatnaveed14@gmail.com>
2026-03-14 02:17:33 +0500

    completed!

--- index.js
+++ index.js
@@ -175,45 +175,246 @@ document.writeln("<h2>Question-18</h2>");
 let text = "The quick brown fox jumps over the lazy dog".toLowerCase();
 let change = text.split(" ");
 console.log(change);
-let flag = 0
-for (let i = 0; i < change.length; i++){
-if (change[i] === "the"){
-flag++
+let flag = 0;
+for (let i = 0; i < change.length; i++) {
+  if (change[i] === "the") {
+    flag++;
+  }
 }
-}console.log(`There are ${flag} occurence(s) of "the"`);
-
+console.log(`There are ${flag} occurence(s) of "the"`);
 
 document.writeln("<h3>Done</h3>");
 
 
 // ********** ASSIGNMENT # 21 - 25 Completed!! **********
 // ----------------------------------------------------------
-
-
+//////////////////////////////////
+document.writeln( `<br> /////////////////////////////completed assignment 21 - 25///////////////////////////////////////////`)
 // ********** ASSIGNMENT # 26 - 30 **********
+// ******* QUESTION-1 ************
+document.writeln("<h2>Question-1</h2>");
+// let userInput = prompt("Enter a positive integer: ");
+// document.writeln(
+//   `Number: ${userInput} <br> Round off value: ${Math.round(userInput)} <br> Floor value: ${Math.floor(userInput)} <br> Ceil value: ${Math.ceil(userInput)}`,
+// );
+
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-2 ************
+
+document.writeln("<h2>Question-2</h2>");
+// let userinput = prompt("Enter a negative floating-point: ");
+// document.writeln(
+//   `Number: ${userinput} <br> Round off value: ${Math.round(userinput)} <br> Floor value: ${Math.floor(userinput)} <br> Ceil value: ${Math.ceil(userinput)}`,
+// );
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-3 ************
+
+document.writeln("<h2>Question-3 </h2>");
+let Number = -4;
+document.writeln(
+  `The absolute value of ${Number} is ${Math.abs(Number)}`,
+);
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-4 ************
+document.writeln("<h2>Question-4</h2>");
+
+let diceRoll = Math.floor(Math.random() * 6) + 1;
+document.writeln(`Random dice value: ${diceRoll}`);
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-5 ************
+document.writeln("<h2>Question-5</h2>");
+
+// let generate_Number = Math.floor( Math.random() * 6)
+// if (generate_Number === 1){
+//   alert(generate_Number + "Random coins value: Tails" )
+// }else{
+//   alert(generate_Number + "Random coins value: Head" )
+// }
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-6 ************
+document.writeln("<h2>Question-6</h2>");
+
+let random_Number = Math.floor(Math.random() * 100) + 1
+document.writeln("random number between 1 and 100 : " + random_Number)
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-7 ************
+
+document.writeln("<h2>Question-7</h2>");
+// let user_weight = prompt("Enter your weight in kiograms")
+// let weight = parseFloat(user_weight)
+// alert(`The weight of user is ${weight} kilograms`)
+document.writeln("<h3>Done</h3>");
 
 
+// ******* QUESTION-8 ************
 
+document.writeln("<h2>Question-8</h2>");
+
+// let secret_number = Math.floor(Math.random() * 10) + 1;
+// let userAns = +prompt("Guess the number b/w 1-10");
+
+// if (userAns === secret_number) {
+//  document.writeln("Congratulations! you win");
+// } else {
+//  document.writeln("Try again: " + secret_number);
+
+// }
+document.writeln("<h3>Done</h3>");
 
 // ********** ASSIGNMENT # 26 - 30 Completed!! **********
 // ----------------------------------------------------------
+//////////////////////////////////
+document.writeln( `<br> /////////////////////////////completed assignment 26 - 30///////////////////////////////////////////`)
 
 // ********** ASSIGNMENT # 31 - 34  **********
+// ******* QUESTION-1 ************
+document.writeln("<h2>Question-1</h2>");
 
+let currentDate = new Date();
+document.writeln( currentDate);
 
+document.writeln("<h3>Done</h3>");
+// ******* QUESTION-2 ************
+document.writeln("<h2>Question-2</h2>");
+let monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+let currentMonth = currentDate.getMonth();
+document.writeln(`Current Month: ${monthNames[currentMonth]}`);
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-3 ************
+document.writeln("<h2>Question-3</h2>");
+// let dayNames = [
+//   "Sunday",
+//   "Monday",
+//   "Tuesday",
+//   "Wednesday",
+//   "Thursday",
+//   "Friday",
+//   "Saturday"
+// ];
+// let currentDay = currentDate.getDay();
+// document.writeln(`Today is: ${dayNames[currentDay].slice(0, 3)}`); 
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-4 ************
+document.writeln("<h2>Question-4</h2>");
+let dayNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let currentDay = currentDate.getDay();
+let day = dayNames[currentDay].slice(0, 3);
+if (day === "Sat" || day === "Sun") {
+  document.writeln("It's Fun day");
+} else {
+  document.writeln("It's a Weekday");
+}
+document.writeln("<h3>Done</h3>");
+// ******* QUESTION-5 ************
+document.writeln("<h2>Question-5</h2>");
+let   date = currentDate.getDate();
+if (date < 16) {
+  document.writeln("First fifteen days of the month");
+} else {
+  document.writeln("Last days of the month");
+}
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-6 ************
+document.writeln("<h2>Question-6</h2>");
+let current_Date = new Date()
+let milisecond = current_Date.getTime()
+let minutes = milisecond / 60000;
+
+document.writeln("Current Date: " + current_Date + "<br>");
+document.writeln("Milliseconds since Jan 1, 1970: " + milisecond + "<br>");
+document.writeln("Minutes since Jan 1, 1970: " + minutes);
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-7 ************
+document.writeln("<h2>Question-7</h2>");
+
+// let hours = current_Date.getHours()
+// if (hours < 12){
+// alert("Its AM.")
+// }else if (hours >= 12){
+// alert("Its PM.")
+// }
+
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-8 ************
+document.writeln("<h2>Question-8</h2>");
+let laterDate = new Date("December 31, 2020");
+document.writeln(laterDate);
+
+document.writeln("<h3>Done</h3>");
+// ******* QUESTION-9 ************
+document.writeln("<h2>Question-9</h2>");
+document.writeln("<h3>Done</h3>");
+// ******* QUESTION-10 ************
+document.writeln("<h2>Question-10</h2>");
+document.writeln("<h3>Done</h3>");
+// ******* QUESTION-11 ************
+document.writeln("<h2>Question-11</h2>");
+document.writeln("<h3>Done</h3>");
+// ******* QUESTION-12 ************
+document.writeln("<h2>Question-12</h2>");
+document.writeln("<h3>Done</h3>");
+// ******* QUESTION-13 ************
+document.writeln("<h2>Question-13</h2>");
+document.writeln("<h3>Done</h3>");
+// ******* QUESTION-14 ************
+document.writeln("<h2>Question-14</h2>");
+document.writeln("<h3>Done</h3>");
 
 // ********** ASSIGNMENT # 31 - 34  Completed!! **********
 // ----------------------------------------------------------
+//////////////////////////////////
+document.writeln( `<br> /////////////////////////////completed assignment 31 - 34///////////////////////////////////////////`)
 
 // ********** ASSIGNMENT # 35 - 38 **********
-
-
+// ******* QUESTION- ************
+document.writeln("<h2>Question-</h2>");
+document.writeln("<h3>Done</h3>");
 
 // ********** ASSIGNMENT # 35 - 38 Completed!! **********
 // ----------------------------------------------------------
+//////////////////////////////////
+document.writeln( `<br> /////////////////////////////completed assignment 35 - 38///////////////////////////////////////////`)
 
 // ********** ASSIGNMENT # 39 - 42 **********
-
+// ******* QUESTION- ************
+document.writeln("<h2>Question-</h2>");
+document.writeln("<h3>Done</h3>");
 
 // ********** ASSIGNMENT # 39 - 42 Completed!! **********
 // ----------------------------------------------------------
+//////////////////////////////////
+document.writeln( `<br> /////////////////////////////completed assignment 39 - 42///////////////////////////////////////////`)
+
+
