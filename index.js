66f54423cd6e64887da8a27bda67f4eaf8294c32
Mishkat Naveed <mishkatnaveed14@gmail.com>
2026-03-13 23:10:23 +0500

    completed!

---
+++ index.js
@@ -0,0 +1,219 @@
+// ********** ASSIGNMENT # 21 - 25 **********
+
+document.writeln("<h1>Assignment # 21 - 25</h1>");
+// ******* QUESTION-1 ************
+document.writeln("<h2>Question-1</h2>");
+// let FirstName = prompt("Enter the First Name : ");
+// let LastName = prompt("Enter the Last Name : ");
+// let fullName = FirstName + " " + LastName;
+// alert("Hello..." + " " + fullName);
+
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-2 ************
+document.writeln("<h2>Question-2</h2>");
+
+// let favMobile = prompt("Enter your Favourite Mobile Phone Model");
+// let mobileLength = favMobile.length;
+// document.writeln(`My favourite phone is: ${favMobile} <br> Length of string: ${mobileLength} `);
+
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-3 ************
+document.writeln("<h2>Question-3</h2>");
+
+let countryName = "Pakistani";
+let index = countryName.indexOf("n");
+document.writeln(`String: ${countryName} <br> Index of 'n': ${index} `);
+
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-4 ************
+document.writeln("<h2>Question-4</h2>");
+
+let word = "Hello World";
+let lastIndex = word.lastIndexOf("l");
+document.writeln(`String: ${word} <br> Last Index of 'l': ${lastIndex} `);
+
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-5 ************
+document.writeln("<h2>Question-5</h2>");
+
+let str = "Pakistani";
+let char = str.charAt(3);
+document.writeln(`String: ${str} <br> Character at index 3: ${char}`);
+
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-6 ************
+document.writeln("<h2>Question-6</h2>");
+
+// let FirstName = prompt("Enter the First Name : ");
+// let LastName = prompt("Enter the Last Name : ");
+// let fullName = FirstName.concat(" ", LastName);
+// alert("Hello..." + " " + fullName);
+
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-7 ************
+document.writeln("<h2>Question-7</h2>");
+
+let city = "Hyderabad";
+let replaceCity = city.replace("Hyder", "Islam");
+document.writeln(`City: ${city} <br> After Replacement: ${replaceCity}`);
+
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-8 ************
+document.writeln("<h2>Question-8</h2>");
+
+let message =
+  " Ali and Sami are best friends. They play cricket and football together.";
+let replace_message = message.replace(/and/gi, "&");
+document.writeln(replace_message);
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-9 ************
+document.writeln("<h2>Question-9</h2>");
+
+let number = 472;
+console.log("value:" + number);
+console.log(typeof number);
+
+console.log("value:" + number);
+let numberAsString = number.toString();
+console.log(typeof numberAsString);
+
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-10 ************
+document.writeln("<h2>Question-10</h2>");
+// let Userinput = prompt("Enter the word which would be converted in uppercase: ")
+// let upper_input = Userinput.toUpperCase()
+// alert(`User input: ${Userinput} \nUpper Case: ${upper_input}`)
+
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-11 ************
+document.writeln("<h2>Question-11</h2>");
+
+// let userInput = prompt("Enter the word which would be converted in Title case: ")
+// let user = userInput[0].toUpperCase() + userInput.slice(1)
+// console.log(user);
+
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-12 ************
+document.writeln("<h2>Question-12</h2>");
+
+var num = 35.36;
+console.log(num.toString().replace(".", ""));
+
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-13 ************
+document.writeln("<h2>Question-13</h2>");
+
+// let user = prompt("Enter the User name: ");
+// let flag = 0;
+// for (let i = 0; i < user.length; i++) {
+//   let chr = user.charCodeAt(i);
+//   if (chr == 33 || chr == 44 || chr == 46 || chr == 64) {
+//     alert("Please Enter a valid Username");
+//    flag = 1;
+//   }
+// }
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-14 ************
+document.writeln("<h2>Question-14</h2>");
+
+// let bakeryItems = ["cake", "apple pie", "cookie", "chips", "patties"];
+// let searchInput = prompt(
+//   "Welcome to ABC Bakery. What do you want to order Sir / Ma'am?",
+// ).toLowerCase();
+// let flag = 0;
+// for (let i = 0; i < bakeryItems.length; i++) {
+//   if (bakeryItems[i] === searchInput) {
+//     alert(
+//       `${searchInput} is available at index ${bakeryItems.indexOf(searchInput)}`,
+//     );
+//     flag = 1;
+//     break;
+//   }
+// }
+// if (flag === 0) {
+//   alert(`We are sorry. ${searchInput} is not available in our bakery..`);
+// }
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-15 ************
+document.writeln("<h2>Question-15</h2>");
+
+// let user_pswd = prompt(`Enter the Valid Password and Password must be:
+
+document.writeln("<h3>Done</h3>");
+
+// ******* QUESTION-16 ************
+document.writeln("<h2>Question-16</h2>");
+
+var university = "University of Karachi";
+console.log(university.split(""));
+
+document.writeln("<h3>Done</h3>");
+// ******* QUESTION-17 ************
+document.writeln("<h2>Question-17</h2>");
+
+// let input = prompt("Enter the name: ")
+// alert(input.slice(-1))
+
+document.writeln("<h3>Done</h3>");
+// ******* QUESTION-18 ************
+document.writeln("<h2>Question-18</h2>");
+
+let text = "The quick brown fox jumps over the lazy dog".toLowerCase();
+let change = text.split(" ");
+console.log(change);
+let flag = 0
+for (let i = 0; i < change.length; i++){
+if (change[i] === "the"){
+flag++
+}
+}console.log(`There are ${flag} occurence(s) of "the"`);
+
+
+document.writeln("<h3>Done</h3>");
+
+
+// ********** ASSIGNMENT # 21 - 25 Completed!! **********
+// ----------------------------------------------------------
+
+
+// ********** ASSIGNMENT # 26 - 30 **********
+
+
+
+
+// ********** ASSIGNMENT # 26 - 30 Completed!! **********
+// ----------------------------------------------------------
+
+// ********** ASSIGNMENT # 31 - 34  **********
+
+
+
+// ********** ASSIGNMENT # 31 - 34  Completed!! **********
+// ----------------------------------------------------------
+
+// ********** ASSIGNMENT # 35 - 38 **********
+
+
+
+// ********** ASSIGNMENT # 35 - 38 Completed!! **********
+// ----------------------------------------------------------
+
+// ********** ASSIGNMENT # 39 - 42 **********
+
+
+// ********** ASSIGNMENT # 39 - 42 Completed!! **********
+// ----------------------------------------------------------
